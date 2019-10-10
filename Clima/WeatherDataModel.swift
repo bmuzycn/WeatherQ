28ae9e81264699ddddfc73f6ec2f811c55244ded
YU ZHANG <44820604+bmuzycn@users.noreply.github.com>
2019-10-09 20:16:29 -0500

    Update WeatherDataModel.swift

--- Clima/WeatherDataModel.swift
+++ Clima/WeatherDataModel.swift
@@ -2,9 +2,8 @@
 //  WeatherDataModel.swift
 //  WeatherApp
 //
-//  Created by Angela Yu on 24/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
+//  Created by Yu Zhang on 04/08/2019.
+
 
 import UIKit
 
